1e6a9feb92fbb10387102eb58c639694d3bdf1d1
Henrik Vesterinen <henrikv@iki.fi>
2018-08-13 14:27:32 +0300

    initial commit
    Signed-off-by: Henrik Vesterinen <henrikv@iki.fi>

---
+++ server.js
@@ -0,0 +1,59 @@
+
+// launch server: nodemon server.js -e js,hbs
+
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+// custom middleware:
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  let logFileName = "server.log";
+  console.log(log);
+  fs.appendFile(logFileName, log + "\n", (err) => {
+    if (err) {
+      console.log(`Unable to append to ${logFileName}`);
+    }
+  });
+  next();  // next tells express when middleware "is done"
+});
+
+// blocking "maintenance" middleware without next:
+app.use((req, res, next) => {
+  res.render("maintenance.hbs");
+});
+
+// NB: use static folder only after the "maintenance" middleware
+// otherwise it will be displayed even though it shouldn't.
+app.use(express.static(__dirname + "/public"));
+hbs.registerHelper("getCurrentYear", () => new Date().getFullYear());
+hbs.registerHelper("screamIt", (text) => {
+  return text.toUpperCase();
+});
+
+app.get("/", (request, response) => {
+  response.render("home.hbs", {
+    pageTitle: "Home page",
+    welcomeMessage: "Terwetuloa sivuille",
+  });
+});
+
+app.get("/about", (request, response) => {
+  response.render("about.hbs", {
+    pageTitle: "About page",
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res.send({status: "Error", errorMessage: "Bad page reached."});
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
